a74693d27aaec64cb07a2b9b6cd690eed842b18d
Julian Waller <git@julusian.co.uk>
2022-05-09 10:52:21 +0100

    feat: initial implementation of applyAndValidateOverrides

--- packages/corelib/src/settings/objectWithOverrides.ts
+++ packages/corelib/src/settings/objectWithOverrides.ts
@@ -1,3 +1,7 @@
+import objectPath = require('object-path')
+import _ = require('underscore')
+import { assertNever, clone } from '../lib'
+
 /**
  * This is an object which allows for overrides to be tracked and reapplied
  * Note: it does not yet support arrays. When one is encountered, that will be treated as a single 'value'
@@ -23,11 +27,7 @@ export interface ObjectOverrideDeleteOp {
 	path: string
 }
 
-// export function diffObjects<T extends object>(base: T, overriden: T): ObjectWithOverrides<T> {
-// 	// TODO
-// }
-
-export interface ApplyOverridesResult<T> {
+export interface ApplyOverridesResult<T extends object> {
 	obj: T
 	/** Overrides which should be preserved */
 	preserve: SomeObjectOverrideOp[]
@@ -37,7 +37,72 @@ export interface ApplyOverridesResult<T> {
 	invalid: SomeObjectOverrideOp[]
 }
 
-export function applyAndValidateOverrides<T extends object>(_obj: ObjectWithOverrides<T>): ApplyOverridesResult<T> {
-	// TODO
-	throw new Error('not implemented')
+function getParentObjectPath(path: string): string | undefined {
+	const lastIndex = path.lastIndexOf('.')
+	if (lastIndex === -1) return undefined
+
+	return path.substring(0, lastIndex)
+}
+
+/**
+ * Combine the ObjectWithOverrides to give the simplified object.
+ * Also performs validation of the overrides, and classifies them
+ * Note: No validation is done to make sure the type conforms to the typescript definition. It is assumed that the definitions which drive ui ensure that they dont violate the typings, and that any changes will be backwards compatible with old overrides
+ */
+export function applyAndValidateOverrides<T extends object>(obj: ObjectWithOverrides<T>): ApplyOverridesResult<T> {
+	const result: ApplyOverridesResult<T> = {
+		obj: clone(obj.defaults),
+		preserve: [],
+		unused: [],
+		invalid: [],
+	}
+
+	// Work through all the overrides
+	for (const override of obj.overrides) {
+		switch (override.op) {
+			case 'set':
+				applySetOp(result, override)
+				break
+			case 'delete':
+				applyDeleteOp(result, override)
+				break
+			default:
+				assertNever(override)
+				result.invalid.push(override)
+				break
+		}
+	}
+
+	return result
+}
+
+function applySetOp<T extends object>(result: ApplyOverridesResult<T>, operation: ObjectOverrideSetOp): void {
+	const parentPath = getParentObjectPath(operation.path)
+	if (parentPath && !objectPath.has(result.obj, parentPath)) {
+		// Parent does not exist in the object, so this is invalid
+		result.invalid.push(operation)
+	} else {
+		result.preserve.push(operation)
+
+		const existingValue = objectPath.get(result.obj, operation.path)
+		if (_.isEqual(existingValue, operation.value)) {
+			// Same value
+			result.unused.push(operation)
+		} else {
+			// Set the new value
+			objectPath.set(result.obj, operation.path, clone(operation.value))
+		}
+	}
+}
+
+function applyDeleteOp<T extends object>(result: ApplyOverridesResult<T>, operation: ObjectOverrideDeleteOp): void {
+	if (objectPath.has(result.obj, operation.path)) {
+		// It exists in the path, so do the delete
+		objectPath.del(result.obj, operation.path)
+	} else {
+		// Track that the op did nothing
+		result.unused.push(operation)
+	}
+	// Always keep the delete op
+	result.preserve.push(operation)
 }
